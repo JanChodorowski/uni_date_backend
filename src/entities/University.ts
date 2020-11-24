6f1338fd576b963d27448ec987c5a50aee32f643
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-24 13:20:17 +0100

    changed University constructor

--- src/entities/University.ts
+++ src/entities/University.ts
@@ -3,39 +3,19 @@ import {
   Entity, PrimaryGeneratedColumn, Column, PrimaryColumn,
 } from 'typeorm';
 
-// @Entity()
-// export class University {
-//     @PrimaryGeneratedColumn()
-//     id!: number;
-
-//     @Column()
-//     firstName!: string;
-
-//     @Column()
-//     lastName!: string;
-
-//     @Column()
-//     age!: number;
-// }
-
 export interface IUniversity {
     name: string;
 }
 
-@Entity({ name: 'university' })
+@Entity()
 class University implements IUniversity {
     @PrimaryColumn()
     public name: string;
 
-    constructor(nameOrUniversity: string) {
-      console.log('HEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY');
-      this.name = nameOrUniversity;
-
-    //   if (typeof nameOrUniversity === 'string') {
-    //     this.name = nameOrUniversity;
-    //   } else {
-    //     // this.name = nameOrUniversity.name || '';
-    //   }
+    constructor(newUniversity: IUniversity) {
+      const { name } = newUniversity || {} as IUniversity;
+      console.log('HEYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY', name);
+      this.name = name;
     }
 }
 
