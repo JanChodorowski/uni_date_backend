54744d8d10875eda74f7915a5036f3758e93cb53
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-24 21:56:37 +0100

    git rm -rf --cached .

---
+++ src/daos/University/UniversityDao.ts
@@ -0,0 +1,62 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+import { IUniversity } from '@entities/University';
+import University from '@entities/University';
+
+import { getConnection } from 'typeorm';
+
+export interface IUniversityDao {
+    // getOne: (email: string) => Promise<IUser | null>;
+    getAll: () => Promise<IUniversity[]>;
+    // add: (user: IUser) => Promise<void>;
+    // update: (user: IUser) => Promise<void>;
+    // delete: (id: number) => Promise<void>;
+}
+
+class UniversityDao implements IUniversityDao {
+  /**
+     * @param name
+     */
+  //   public getOne(email: string): Promise<IUser | null> {
+  //     // TODO
+  //     return Promise.resolve(null);
+  //   }
+
+  /**
+     *
+     */
+  public getAll(): Promise<IUniversity[]> {
+    // TODO
+    // return Promise.resolve([]);
+    return getConnection().manager.find(University);
+  }
+
+  //   /**
+  //      *
+  //      * @param user
+  //      */
+  //   public async add(user: IUser): Promise<void> {
+  //     // TODO
+  //     return Promise.resolve(undefined);
+  //   }
+
+  //   /**
+  //      *
+  //      * @param user
+  //      */
+  //   public async update(user: IUser): Promise<void> {
+  //     // TODO
+  //     return Promise.resolve(undefined);
+  //   }
+
+//   /**
+//      *
+//      * @param id
+//      */
+//   public async delete(id: number): Promise<void> {
+//     // TODO
+//     return Promise.resolve(undefined);
+//   }
+}
+
+export default UniversityDao;
