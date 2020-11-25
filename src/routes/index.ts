435c0a8cb713d90bbf7b88fd39fb6dc95256977c
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-11-25 11:53:41 +0100

    import UniversitiesRouter from './Universities';

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import UserRouter from './Users';
+import UniversitiesRouter from './Universities';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
 router.use('/users', UserRouter);
+router.use('/universities', UniversitiesRouter);
 
 // Export the base-router
 export default router;
