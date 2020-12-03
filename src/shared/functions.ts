40f8446ecede5ef4148fb223928644f6c1556bf0
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-12-03 13:50:01 +0100

    bcrypt jsonwebtoken installed

--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -1,11 +1,9 @@
 import logger from './Logger';
 
 export const pErr = (err: Error) => {
-    if (err) {
-        logger.err(err);
-    }
+  if (err) {
+    logger.err(err);
+  }
 };
 
-export const getRandomInt = () => {
-    return Math.floor(Math.random() * 1_000_000_000_000);
-};
+export const getRandomInt = () => Math.floor(Math.random() * 1_000_000_000_000);
