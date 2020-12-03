425c02e691513e1dae814c2f25720baded5acfa3
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2020-12-03 14:33:07 +0100

    test

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React siema
+          Learn React siema simea
         </a>
       </header>
     </div>
