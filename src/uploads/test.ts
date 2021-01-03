039f9ea591d59e22df0bf458a464b1bf502b2c2b
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2021-01-04 00:10:57 +0100

    Remove files

--- src/uploads/test.ts
+++
@@ -1,3 +0,0 @@
-// TO-DO  transparent,    change password, change email, need password for both. University autocomplete, Reorder pictures, delete pictures, yup validation on client and server
-
-const test = 'test';
