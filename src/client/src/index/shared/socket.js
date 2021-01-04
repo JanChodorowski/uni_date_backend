92e3ddfda0abe86bd645a2be9170a708d8599bdf
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2021-01-04 18:23:57 +0100

    export const socket = io(`http://localhost:${process.env.PORT || 3000}`, {

--- src/client/src/index/shared/socket.js
+++ src/client/src/index/shared/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(`http://localhost:${process.env.PORT || 3000}`, {
   withCredentials: true,
   extraHeaders: {
     "my-custom-header": "abcd",
