2ebca234ffbbc3a6618a1f4c26e7a8d4755d2e3a
Tungetyt <48414404+Tungetyt@users.noreply.github.com>
2021-01-14 22:50:45 +0100

    return <>{isLoading && <LinearProgress style={{position: 'fixed'}}/>}</>;

--- src/client/src/index/App/ProgressShower.js
+++ src/client/src/index/App/ProgressShower.js
@@ -5,7 +5,7 @@ import { LoadingContext } from "../shared/loadingContext";
 const ProgressShower = () => {
   const [isLoading] = useContext(LoadingContext);
 
-  return <>{isLoading && <LinearProgress />}</>;
+  return <>{isLoading && <LinearProgress style={{position: 'fixed'}}/>}</>;
 };
 
 export default ProgressShower;
